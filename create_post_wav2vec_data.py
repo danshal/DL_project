5579c54acc8a761ce5407fe551f1af4c243eef3b
Daniel <dans96@gmail.com>
2021-02-07 21:53:46 +0000

    Added arparse to be able to filter if avgpool is nexxenecessary

--- create_post_wav2vec_data.py
+++ create_post_wav2vec_data.py
@@ -11,6 +11,17 @@ from torchaudio.datasets.utils import (
 )
 from torch import nn
 from helper_functions import sv_helper
+import argparse
+
+#create parser
+my_parser = argparse.ArgumentParser(description='Get audio representations of data_path audio files using wav2vec network. Pay Attention: Your data and dst paths have to be under /home/Daniel/DeepProject/dataset/')
+#add arguments
+my_parser.add_argument('data_path', metavar='data_path', type=str, help='path to audio files to be represented')
+my_parser.add_argument('dst_path', metavar='dst_path', type=str, help='path to save audio representations')
+my_parser.add_argument('is_avgpool', metavar='is_avgpool', type=bool, help='boolean flag. When true -> save avg on all time axis to get representations shape of [512,1]')
+
+args = my_parser.parse_args()
+args = vars(args)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -21,10 +32,12 @@ model.eval()
 
 helper = sv_helper(model)
 
-data_path = '/home/Daniel/DeepProject/dataset/cut_train_data_360/'
-dst_data_path = '/home/Daniel/DeepProject/dataset/cut_train_data_360_full_repr'
+#data_path = '/home/Daniel/DeepProject/dataset/cut_train_data_360/'
+#dst_data_path = '/home/Daniel/DeepProject/dataset/cut_train_data_360_full_repr'
+data_path = args['data_path']
+dst_data_path = args['dst_path']
+is_avgpool = args['is_avgpool']
 rootDir = '/home/Daniel/DeepProject/dataset/'
-waveform_length_in_samples = 3 * 16000
 file_ext = '.flac'
 walker = walk_files(data_path, suffix=file_ext, prefix=False, remove_suffix=True)
 walker = list(walker)
@@ -33,13 +46,13 @@ speaker_utterace_counter = 0
 speaker_counter = 0
 
 try:
-  #if not os.path.exists(f'{dst_data_path}/{speaker_counter}'):
+  if not os.path.exists(f'{dst_data_path}/{speaker_counter}'):
+    os.mkdir(f'{dst_data_path}/{speaker_counter}')
     #print('asdasdf')
     #os.makedirs(f'{dst_data_path}/{speaker_counter}', 0o700)
-  os.mkdir(f'{dst_data_path}/{speaker_counter}')
-except OSError as e:
-  print(e)
-  #pass
+except OSError:
+  print('ERROR delete current dataset folder')
+  pass
 
 
 for i in walker:
@@ -56,8 +69,11 @@ for i in walker:
     waveform, _ = torchaudio.load(file_audio) # Load audio - dont care about sample rate
     waveform = waveform.to(device)
     audio_repr = helper.get_tensor_repr(waveform)
-    #avg_pool = nn.AvgPool1d(audio_repr.shape[2])
-    #avg_repr = avg_pool(audio_repr)
-    avg_repr = torch.squeeze(audio_repr)
+    if is_avgpool == True:
+      avg_pool = nn.AvgPool1d(audio_repr.shape[2])
+      avg_repr = avg_pool(audio_repr)
+      avg_repr = torch.squeeze(avg_repr)
+    else:
+      avg_repr = torch.squeeze(audio_repr)
     torch.save(avg_repr, f'{dst_data_path}/{speaker_counter}/{speaker_counter}-{speaker_utterace_counter}.pt')
     speaker_utterace_counter+=1
